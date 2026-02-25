437a7b87deed96884ac2ce8dc321ee1a81b753d6
Marco Scinetti <marco.scinetti@gmail.com>
2026-02-25 18:14:37 +0100

    Update

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,273 +1,285 @@
-import prisma from '../prisma-client/prismaClient.js';
+import prisma from "../prisma-client/prismaClient.js";
 
 // funzione per la creazione nuovo record
 const insertExpense = async (req, res) => {
-  const newExpense = req.body
-  const currentDate = new Date().toJSON()
-  console.log(`Inserimento nuova spesa: ${newExpense}`)
+  const newExpense = req.body;
+  const currentDate = new Date().toJSON();
+  console.log(`Inserimento nuova spesa: ${newExpense}`);
 
-  console.log(
-   {
-      data: {
-        userid : req.userAuth,
-        category_id : newExpense.category_id,
-        date_create : currentDate,
-        date_update : currentDate,
-        amount: newExpense.amount,
-        note : newExpense.note,
-        date: new Date(newExpense.date).toJSON(),
-        }
-    } 
-  )
+  console.log({
+    data: {
+      userid: req.userAuth,
+      category_id: newExpense.category_id,
+      date_create: currentDate,
+      date_update: currentDate,
+      amount: newExpense.amount,
+      note: newExpense.note,
+      date: new Date(newExpense.date).toJSON(),
+    },
+  });
 
-  try{
-
+  try {
     const expense = await prisma.expense.create({
       data: {
-        userid : newExpense.userid,
-        category_id : newExpense.category_id,
-        date_create : currentDate,
-        date_update : currentDate,
+        userid: newExpense.userid,
+        category_id: newExpense.category_id,
+        date_create: currentDate,
+        date_update: currentDate,
         amount: newExpense.amount,
-        note : newExpense.note,
+        note: newExpense.note,
         date: new Date(newExpense.date).toJSON(),
-        }
-    })
+      },
+    });
 
-    res.status(200).send(expense)
-  } catch(error) {
-    res.status(500).send(`Errore nell'insermnento nuova spesa: ${error}`)
+    res.status(200).send(expense);
+  } catch (error) {
+    res.status(500).send(`Errore nell'insermnento nuova spesa: ${error}`);
   }
-}
+};
+
+// script per inserimento spese di test
+// const currentDate = new Date().toJSON();
+// for (let i = 0; i<=10000; i++) {
+//   await prisma.expense.create({
+//    data: {
+//      userid : 29,
+//      category_id : 1,
+//      date_create : currentDate,
+//      date_update : currentDate,
+//      amount: Math.floor(Math.random() * 100),
+//      note : "Spesa inserita in automatico 2",
+//      date: new Date().toJSON(),
+//      }
+//  })
+// }
 
 // funzione per la lettura di tutte le spese
 const readExpense = async (req, res) => {
-  try{
-    const expense = await prisma.expense.findMany()
-    res.status(200).send(expense)
-  } catch(error) {
-    res.status(500).send(`Errore nella lettura spese : ${error}`)
-  }  
-}
+  try {
+    const expense = await prisma.expense.findMany();
+    res.status(200).send(expense);
+  } catch (error) {
+    res.status(500).send(`Errore nella lettura spese : ${error}`);
+  }
+};
 
 // funzione per la lettura delle spese di un utente
 const readUserExpense = async (req, res) => {
-  try{
-    console.log(req.params.id)
-    console.log("User nella richiesta arrivata dal middleware:", req.userAuth)
+  try {
+    console.log(req.params.id);
+    console.log("User nella richiesta arrivata dal middleware:", req.userAuth);
     const expense = await prisma.expense.findMany({
       where: {
         userid: {
-          equals: req.userAuth
-        }
+          equals: req.userAuth,
+        },
       },
       include: {
         category: {
           select: {
-            category: true
-          }
-        }
+            category: true,
+          },
+        },
       },
-    })
-    res.status(200).send(expense)
-  } catch(error) {
-    res.status(500).send(`Errore nella lettura spese : ${error}`)
-  }  
-}
+    });
+    res.status(200).send(expense);
+  } catch (error) {
+    res.status(500).send(`Errore nella lettura spese : ${error}`);
+  }
+};
 
 // funzione per la lettura delle categorie
 const readCategory = async (req, res) => {
-  try{
-    const category = await prisma.category.findMany()
-    res.status(200).send(category)
-  } catch(error) {
-    res.status(500).send(`Errore nella lettura spese : ${error}`)
-  }  
-}
+  try {
+    const category = await prisma.category.findMany();
+    res.status(200).send(category);
+  } catch (error) {
+    res.status(500).send(`Errore nella lettura spese : ${error}`);
+  }
+};
 
 // funzione per l'update di una o più spese
 const updateExpense = async (req, res) => {
   const currentExpense = req.body;
   const currentDate = new Date().toJSON();
   let expense;
-  try{
-  if (Array.isArray(currentExpense.id)) {
-    expense = await prisma.expense.updateMany({
-      where : {
-        id : { in: currentExpense.id },
-        userid: req.userAuth
-      },
-      data : {
-        category_id : currentExpense.category_id,
-        date_update : currentDate,
-        amount : currentExpense.amount,
-        note : currentExpense.note,
-        date: new Date(currentExpense.date).toJSON()
-      }
-    })
-  } else {
-    expense = await prisma.expense.update({
-      where : {
-        id : currentExpense.id,
-        userid: req.userAuth
-      },
-      data : {
-        category_id : currentExpense.category_id,
-        date_update : currentDate,
-        amount : currentExpense.amount,
-        note : currentExpense.note,
-        date: new Date(currentExpense.date).toJSON()
-      }
-    })
+  try {
+    if (Array.isArray(currentExpense.id)) {
+      expense = await prisma.expense.updateMany({
+        where: {
+          id: { in: currentExpense.id },
+          userid: req.userAuth,
+        },
+        data: {
+          category_id: currentExpense.category_id,
+          date_update: currentDate,
+          amount: currentExpense.amount,
+          note: currentExpense.note,
+          date: new Date(currentExpense.date).toJSON(),
+        },
+      });
+    } else {
+      expense = await prisma.expense.update({
+        where: {
+          id: currentExpense.id,
+          userid: req.userAuth,
+        },
+        data: {
+          category_id: currentExpense.category_id,
+          date_update: currentDate,
+          amount: currentExpense.amount,
+          note: currentExpense.note,
+          date: new Date(currentExpense.date).toJSON(),
+        },
+      });
+    }
+    res.status(200).send(expense);
+  } catch (error) {
+    res.status(500).send(`Errore nell'update della spesa: ${error}`);
   }
-    res.status(200).send(expense)
-  } catch(error)
-  {
-    res.status(500).send(`Errore nell'update della spesa: ${error}`)
-  }
-}
+};
 
 // funzione per l'update del budget utente
 const updateBudget = async (req, res) => {
-  const currentDate = new Date().toJSON()
-  const budget = (new Number(req.body.budget)).valueOf()
-  try{
+  const currentDate = new Date().toJSON();
+  const budget = new Number(req.body.budget).valueOf();
+  try {
     const userSettings = await prisma.user_settings.updateMany({
-      where : {
+      where: {
         userid: {
-          equals: req.userAuth
-        }
+          equals: req.userAuth,
+        },
       },
-      data : {
-        date_update : currentDate,
-        budget : budget,
-      }
-    })
-    res.status(200).send(userSettings)
-  } catch(error)
-  {
-    res.status(500).send(`Errore nell'update dell budget: ${error}`)
+      data: {
+        date_update: currentDate,
+        budget: budget,
+      },
+    });
+    res.status(200).send(userSettings);
+  } catch (error) {
+    res.status(500).send(`Errore nell'update dell budget: ${error}`);
   }
-}
+};
 
 // funzione per l'eliminazione di una o più spese
 const deleteExpense = async (req, res) => {
   const currentExpense = req.body;
   let expense;
-  try{
-  if (Array.isArray(currentExpense.id)) {
-    expense = await prisma.expense.deleteMany({
-      where: {
-        id: {
-          in: currentExpense.id
+  try {
+    if (Array.isArray(currentExpense.id)) {
+      expense = await prisma.expense.deleteMany({
+        where: {
+          id: {
+            in: currentExpense.id,
+          },
+          userid: {
+            equals: req.userAuth,
+          },
         },
-        userid: {
-          equals: req.userAuth
-        }
-      }
-    })
+      });
+    } else {
+      expense = await prisma.expense.delete({
+        where: {
+          id: currentExpense.id,
+          userid: req.userAuth,
+        },
+      });
+    }
+    res.status(200).send(expense);
+  } catch (error) {
+    res.status(500).send(`Errore nell'eliminazione della spesa : ${error}`);
   }
-  else {
-    expense = await prisma.expense.delete({
-      where : {
-        id : currentExpense.id,
-        userid: req.userAuth
-      }
-    })
-  }
-    res.status(200).send(expense)
-  } catch(error){
-    res.status(500).send(`Errore nell'eliminazione della spesa : ${error}`)
-  }
-}
+};
 
 // funzione per l'eliminazione di tutte le spese
 const deleteExpenseAll = async (req, res) => {
-  try{
-    const expense = await prisma.expense.deleteMany()
-    res.status(200).send(expense)
-  } catch(error){
-    res.status(500).send(`Errore nell'eliminazione di tutte le spese : ${error}`)
+  try {
+    const expense = await prisma.expense.deleteMany();
+    res.status(200).send(expense);
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Errore nell'eliminazione di tutte le spese : ${error}`);
   }
-}
+};
 
 const readUserKpi = async (req, res) => {
-  try{
-    console.log("Utente per la lettura dei kpi: ", req.userAuth)
+  try {
+    console.log("Utente per la lettura dei kpi: ", req.userAuth);
     // cerco il totale delle spes per l'utente
     const { _sum } = await prisma.expense.aggregate({
-      _sum : {
-        amount: true
+      _sum: {
+        amount: true,
       },
       where: {
         userid: {
-          equals: req.userAuth
-        }
-      }
-    })
+          equals: req.userAuth,
+        },
+      },
+    });
 
     // cerco la categoria in cui l'utente ha speso di più
     const maxCategory = await prisma.expense.groupBy({
       by: ["category_id"],
       where: {
         userid: {
-          equals: req.userAuth
-        }
+          equals: req.userAuth,
+        },
       },
-      _sum : {
-        amount: true
+      _sum: {
+        amount: true,
       },
-    })
+    });
 
-    maxCategory.sort((a ,b) => b._sum.amount - a._sum.amount)
+    maxCategory.sort((a, b) => b._sum.amount - a._sum.amount);
 
     // cerco il nome della categoria con l'id trovato
 
     const maxCategoryDesc = await prisma.category.findFirst({
       where: {
         id: {
-          equals: maxCategory.length > 0 ? maxCategory[0].category_id : -1
-        }
+          equals: maxCategory.length > 0 ? maxCategory[0].category_id : -1,
+        },
       },
       select: {
-        category: true
-      }
-    })
+        category: true,
+      },
+    });
 
     //cerco il budget dell'utente
 
     const budget = await prisma.user_settings.findFirst({
       where: {
         userid: {
-          equals: req.userAuth
-        }
+          equals: req.userAuth,
+        },
       },
       select: {
-        budget: true
-      }
-    })
+        budget: true,
+      },
+    });
 
     res.status(200).send({
-      total: _sum.amount ? _sum.amount : 0, 
+      total: _sum.amount ? _sum.amount : 0,
       maxCategory: {
-        id: maxCategory.length > 0 ? maxCategory[0].category_id : -1, 
-        desc: maxCategoryDesc ? maxCategoryDesc.category : "", 
-        amount: maxCategory.length > 0 ? maxCategory[0]._sum.amount : 0 
+        id: maxCategory.length > 0 ? maxCategory[0].category_id : -1,
+        desc: maxCategoryDesc ? maxCategoryDesc.category : "",
+        amount: maxCategory.length > 0 ? maxCategory[0]._sum.amount : 0,
       },
-      budget: budget.budget 
-    })
-
-  } catch(error) {
-    res.status(500).send(`Errore nella lettura dei Kpi utente: ${error}`)
+      budget: budget.budget,
+    });
+  } catch (error) {
+    res.status(500).send(`Errore nella lettura dei Kpi utente: ${error}`);
   }
-}
+};
 
 const readUserStats = async (req, res) => {
-  try{
-
+  try {
     // leggo l'ammontare per categoria usando una query sql perchè Prisma non implementa l'inclusione dei campi con relazione (category nella taballa category) quando uso il metodo prisma.table.groupBy
 
     // capire se devo stare attento a possibile query injections
+
+    // ricavo i dati aggregando anche su mese e anno
     const categoryAmount = await prisma.$queryRaw`
     SELECT a.category_id,
       b.category,
@@ -283,9 +295,9 @@ const readUserStats = async (req, res) => {
     GROUP BY 
       a.category_id,
       b.category
-    `
+    `;
 
-    // leggo l'ammontare per mese
+    // leggo l'ammontare per mese e categoria
     const monthlyAmount = await prisma.$queryRaw`
       SELECT to_char(date, 'YYYY-MM') as yearMonth,
         SUM(amount) as amount
@@ -293,25 +305,26 @@ const readUserStats = async (req, res) => {
       WHERE 
         userid = ${req.userAuth}
       GROUP BY
-        yearMonth`
+        yearMonth`;
 
-  res.status(200).send({categoryAmount, monthlyAmount})
-    
-  } catch(error) {
-    res.status(500).send(`Errore nella lettura delle statistiche utente: ${error}`)
+    res.status(200).send({ categoryAmount, monthlyAmount });
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Errore nella lettura delle statistiche utente: ${error}`);
   }
-}
+};
 
 // export delle funzioni per CRUD API
-export { 
-  insertExpense, 
-  readExpense, 
+export {
+  insertExpense,
+  readExpense,
   readUserExpense,
-  readCategory, 
+  readCategory,
   updateExpense,
-  updateBudget, 
-  deleteExpense, 
+  updateBudget,
+  deleteExpense,
   deleteExpenseAll,
   readUserKpi,
-  readUserStats
-}
+  readUserStats,
+};
